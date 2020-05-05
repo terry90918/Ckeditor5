a9fd3d812a427ccecdf54ade412d19b61f10189f
terry.chen <zxtw17985321@gmail.com>
2020-05-05 18:23:50 +0800

    style: format

--- src/components/Ckeditor4.vue
+++ src/components/Ckeditor4.vue
@@ -12,74 +12,82 @@
 </template>
 
 <script>
-import CKEditor from '@/utils/ckeditor4-vue'
+import CKEditor from "@/utils/ckeditor4-vue";
 
 export default {
-  name: 'Ckeditor4',
-  data () {
+  name: "Ckeditor4",
+  data() {
     return {
-      editorData: '<p>Content of the editor.</p>',
+      editorData: "<p>Content of the editor.</p>",
       editorConfig: {
-        extraPlugins: 'uploadimage',
-        filebrowserUploadUrl: 'http://localhost:8079/upload',
-        removeButtons: 'Subscript,Superscript,Styles,About,Blockquote,RemoveFormat,Source,Maximize,HorizontalRule,SpecialChar,Anchor,Scayt,Undo,Cut,Copy,Redo,Paste,PasteText,PasteFromWord',
-        removeDialogTabs: 'image:Link;image:advanced;link:target;link:upload;link:advanced',
+        extraPlugins: "uploadimage",
+        filebrowserUploadUrl: "http://localhost:8079/upload",
+        removeButtons:
+          "Subscript,Superscript,Styles,About,Blockquote,RemoveFormat,Source,Maximize,HorizontalRule,SpecialChar,Anchor,Scayt,Undo,Cut,Copy,Redo,Paste,PasteText,PasteFromWord",
+        removeDialogTabs:
+          "image:Link;image:advanced;link:target;link:upload;link:advanced",
         toolbarGroups: [
-          { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-          { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-          { name: 'styles', groups: [ 'styles' ] },
-          { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-          { name: 'forms', groups: [ 'forms' ] },
-          { name: 'tools', groups: [ 'tools' ] },
-          { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-          { name: 'others', groups: [ 'others' ] },
-          { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-          { name: 'links', groups: [ 'links' ] },
-          { name: 'insert', groups: [ 'insert' ] },
-          { name: 'colors', groups: [ 'colors' ] },
-          { name: 'about', groups: [ 'about' ] }
+          { name: "clipboard", groups: ["clipboard", "undo"] },
+          {
+            name: "editing",
+            groups: ["find", "selection", "spellchecker", "editing"]
+          },
+          { name: "styles", groups: ["styles"] },
+          { name: "basicstyles", groups: ["basicstyles", "cleanup"] },
+          { name: "forms", groups: ["forms"] },
+          { name: "tools", groups: ["tools"] },
+          { name: "document", groups: ["mode", "document", "doctools"] },
+          { name: "others", groups: ["others"] },
+          {
+            name: "paragraph",
+            groups: ["list", "indent", "blocks", "align", "bidi", "paragraph"]
+          },
+          { name: "links", groups: ["links"] },
+          { name: "insert", groups: ["insert"] },
+          { name: "colors", groups: ["colors"] },
+          { name: "about", groups: ["about"] }
         ],
-        imageUploadUrl: 'http://localhost:8079/upload' // Adding drag and drop image upload.
+        imageUploadUrl: "http://localhost:8079/upload" // Adding drag and drop image upload.
       }
-    }
+    };
   },
   components: {
     ckeditor: CKEditor.component
   },
   methods: {
     onEditorReady(evt) {
-      console.log('Editor is ready.', { evt })
+      console.log("Editor is ready.", { evt });
     },
     onEditorFocus(evt) {
-      console.log('Editor focused.', { evt })
+      console.log("Editor focused.", { evt });
     },
     onEditorBlur(evt) {
-      console.log('Editor blurred.', { evt })
+      console.log("Editor blurred.", { evt });
     },
     onEditorInput(evt) {
-      console.log('Editor data input.', { evt })
+      console.log("Editor data input.", { evt });
     },
     onFileUploadRequest(evt) {
-      console.log('File Upload.', { evt })
+      console.log("File Upload.", { evt });
     },
     onFileUploadResponse(evt) {
-      console.log('File Uploaded.', { evt })
+      console.log("File Uploaded.", { evt });
       evt.stop();
 
       // Get XHR and response.
-      var data = evt.data,
-          xhr = data.fileLoader.xhr,
-          response = xhr.responseText.split( '|' );
+      let data = evt.data;
+      const xhr = data.fileLoader.xhr;
+      const response = xhr.responseText.split("|");
       const temp1 = JSON.parse(response);
 
-      if ( response[ 1 ] ) {
+      if (!temp1.success) {
         // An error occurred during upload.
-        data.message = response[ 1 ];
+        data.message = temp1.msg;
         evt.cancel();
       } else {
         data.url = temp1.data.url;
       }
-    },
+    }
   }
-}
+};
 </script>
--- src/utils/ckeditor4-vue/ckeditor.js
+++ src/utils/ckeditor4-vue/ckeditor.js
@@ -8,146 +8,146 @@
 import { getEditorNamespace } from './geteditornamespace.js';
 
 export default {
-	name: 'ckeditor',
+  name: 'ckeditor',
 
-	render(createElement) {
-		return createElement('div', {}, [
-			createElement(this.tagName)
-		]);
-	},
+  render(createElement) {
+    return createElement('div', {}, [
+      createElement(this.tagName)
+    ]);
+  },
 
-	props: {
-		value: {
-			type: String,
-			default: ''
-		},
-		type: {
-			type: String,
-			default: 'classic',
-			validator: type => ['classic', 'inline'].includes(type)
-		},
-		editorUrl: {
-			type: String,
-			default: 'https://cdn.ckeditor.com/4.14.0/standard-all/ckeditor.js'
-		},
-		config: {
-			type: Object,
-			default: () => { }
-		},
-		tagName: {
-			type: String,
-			default: 'textarea'
-		},
-		readOnly: {
-			type: Boolean,
-			default: null // Use null as the default value, so `config.readOnly` can take precedence.
-		}
-	},
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'classic',
+      validator: type => ['classic', 'inline'].includes(type)
+    },
+    editorUrl: {
+      type: String,
+      default: 'https://cdn.ckeditor.com/4.14.0/standard-all/ckeditor.js'
+    },
+    config: {
+      type: Object,
+      default: () => { }
+    },
+    tagName: {
+      type: String,
+      default: 'textarea'
+    },
+    readOnly: {
+      type: Boolean,
+      default: null // Use null as the default value, so `config.readOnly` can take precedence.
+    }
+  },
 
-	mounted() {
-		getEditorNamespace(this.editorUrl).then(() => {
-			if (this.$_destroyed) {
-				return;
-			}
+  mounted() {
+    getEditorNamespace(this.editorUrl).then(() => {
+      if (this.$_destroyed) {
+        return;
+      }
 
-			const config = this.config || {};
+      const config = this.config || {};
 
-			if (this.readOnly !== null) {
-				config.readOnly = this.readOnly;
-			}
+      if (this.readOnly !== null) {
+        config.readOnly = this.readOnly;
+      }
 
-			const method = this.type === 'inline' ? 'inline' : 'replace';
-			const element = this.$el.firstElementChild;
-			const editor = this.instance = CKEDITOR[method](element, config);
+      const method = this.type === 'inline' ? 'inline' : 'replace';
+      const element = this.$el.firstElementChild;
+      const editor = this.instance = CKEDITOR[method](element, config);
 
-			editor.on('instanceReady', () => {
-				const data = this.value;
+      editor.on('instanceReady', () => {
+        const data = this.value;
 
-				editor.fire('lockSnapshot');
+        editor.fire('lockSnapshot');
 
-				editor.setData(data, {
-					callback: () => {
-						this.$_setUpEditorEvents();
+        editor.setData(data, {
+          callback: () => {
+            this.$_setUpEditorEvents();
 
-						const newData = editor.getData();
+            const newData = editor.getData();
 
-						// Locking the snapshot prevents the 'change' event.
-						// Trigger it manually to update the bound data.
-						if (data !== newData) {
-							this.$once('input', () => {
-								this.$emit('ready', editor);
-							});
+            // Locking the snapshot prevents the 'change' event.
+            // Trigger it manually to update the bound data.
+            if (data !== newData) {
+              this.$once('input', () => {
+                this.$emit('ready', editor);
+              });
 
-							this.$emit('input', newData);
-						} else {
-							this.$emit('ready', editor);
-						}
+              this.$emit('input', newData);
+            } else {
+              this.$emit('ready', editor);
+            }
 
-						editor.fire('unlockSnapshot');
-					}
-				});
-			});
-		});
-	},
+            editor.fire('unlockSnapshot');
+          }
+        });
+      });
+    });
+  },
 
-	beforeDestroy() {
-		if (this.instance) {
-			this.instance.destroy();
-		}
+  beforeDestroy() {
+    if (this.instance) {
+      this.instance.destroy();
+    }
 
-		this.$_destroyed = true;
-	},
+    this.$_destroyed = true;
+  },
 
-	watch: {
-		value(val) {
-			if (this.instance.getData() !== val) {
-				this.instance.setData(val);
-			}
-		},
+  watch: {
+    value(val) {
+      if (this.instance.getData() !== val) {
+        this.instance.setData(val);
+      }
+    },
 
-		readOnly(val) {
-			this.instance.setReadOnly(val);
-		}
-	},
+    readOnly(val) {
+      this.instance.setReadOnly(val);
+    }
+  },
 
-	methods: {
-		$_setUpEditorEvents() {
-			const editor = this.instance;
+  methods: {
+    $_setUpEditorEvents() {
+      const editor = this.instance;
 
-			editor.on('change', evt => {
-				this.onChange(evt)
-			});
+      editor.on('change', evt => {
+        this.onChange(evt)
+      });
 
-			editor.on('focus', evt => {
-				this.$emit('focus', evt, editor);
-			});
+      editor.on('focus', evt => {
+        this.$emit('focus', evt, editor);
+      });
 
-			editor.on('blur', evt => {
-				this.$emit('blur', evt, editor);
-			});
+      editor.on('blur', evt => {
+        this.$emit('blur', evt, editor);
+      });
 
-			// Ckeditor file upload request event
-			editor.on('fileUploadRequest', evt => {
-				this.$emit('fileUploadRequest', evt);
-			});
+      // Ckeditor file upload request event
+      editor.on('fileUploadRequest', evt => {
+        this.$emit('fileUploadRequest', evt);
+      });
 
-			// Ckditor file upload response event
-			editor.on('fileUploadResponse', evt => {
-				setTimeout(() => {
-					this.onChange();
-				}, 0);
-				this.$emit('fileUploadResponse', evt);
-			});
-		},
-		onChange(evt) {
-			const editor = this.instance;
-			const data = editor.getData();
+      // Ckditor file upload response event
+      editor.on('fileUploadResponse', evt => {
+        setTimeout(() => {
+          this.onChange();
+        }, 0);
+        this.$emit('fileUploadResponse', evt);
+      });
+    },
+    onChange(evt) {
+      const editor = this.instance;
+      const data = editor.getData();
 
-			// Editor#change event might be fired without an actual data change.
-			if (this.value !== data) {
-				// The compatibility with the v-model and general Vue.js concept of input–like components.
-				this.$emit('input', data, evt, editor);
-			}
-		}
-	}
+      // Editor#change event might be fired without an actual data change.
+      if (this.value !== data) {
+        // The compatibility with the v-model and general Vue.js concept of input–like components.
+        this.$emit('input', data, evt, editor);
+      }
+    }
+  }
 };
